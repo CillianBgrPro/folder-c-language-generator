ffc60e812b74f8c9e3a164302432467f69018f75
Cillian <baugercillianpro@gmail.com>
2025-10-16 15:07:04 +0200

    c folder generator

--- generator.sh
+++ generator.sh
@@ -8,6 +8,9 @@ echo "Dossier du projet créé."
 cd $dossier
 touch main.c
 echo "Fichier main.c créé."
+echo "Quelle est le nom de vos fichiers ?"
+read project_name
+fichier="project_name"
 cat <<EOF > main.c
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,4 +19,15 @@ int main(){
 
     return(0);
 }
+EOF
+
+touch ./$project_name.c
+touch ./$project_name.h
+echo "projet .c et .h créé."
+
+cat <<EOF > $project_name.c
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+#endif
 EOF
